b2b91c6b4a6f320a9f6cd8f84067fb569c78f167
Krishna <gobi.jeyapal@brainiuminfotech.com>
2022-11-29 20:32:56 +0530

    redux login code pushed

--- src/store/Store.js
+++ src/store/Store.js
@@ -5,9 +5,11 @@ import ChatsReducer from './apps/chat/ChatSlice';
 import ContactsReducer from './apps/contacts/ContactSlice';
 import EmailReducer from './apps/email/EmailSlice';
 import TicketReducer from './apps/ticket/TicketSlice';
+import userReducer from './auth/userSlice';
 
 export const store = configureStore({
   reducer: {
+    user:userReducer,
     customizer: CustomizerReducer,
     notesReducer: NotesReducer,
     chatReducer: ChatsReducer,
