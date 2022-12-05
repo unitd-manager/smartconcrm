bba9e67cdb586c1c571e73e6873ceb7029c009b5
rafiunited <113578730+rafiunited@users.noreply.github.com>
2022-12-05 16:15:41 +0530

    client.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://43.228.126.245:3001',
+  // baseURL: 'http://43.228.126.245:3001',
+  baseURL: 'http://localhost:3001',
+
   //   timeout: 1000,
   headers: { 'X-Custom-Header': 'foobar' },
 });
