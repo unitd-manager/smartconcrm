7d810e2669ea377ffdb9a9c576a04257b3bda93a
fatemauts <113575341+fatemauts@users.noreply.github.com>
2022-12-17 10:18:14 +0530

    delete delivery order/data dynamic in edit modal

--- src/views/EditData/TenderEdit.js
+++ src/views/EditData/TenderEdit.js
@@ -16,11 +16,11 @@ import EditLineItemModal from '../../components/tender/EditLineItemModal';
 // import GeneratePDFPrint from '../../components/tender/GeneratePDFPrint';
 // import ViewLineItemModal from '../../components/tender/ViewLineItemModal';
 import AttachmentModal from '../../components/tender/AttachmentModal';
+import ViewFileComponent from '../../components/ProjectModal/ViewFileComponent';
 import BreadCrumbs from '../../layouts/breadcrumbs/BreadCrumbs';
 import ComponentCard from '../../components/ComponentCard';
 import message from '../../components/Message';
 import api from '../../constants/api';
-
 
 const TenderEdit = () => {
 
@@ -42,8 +42,6 @@ const TenderEdit = () => {
     const [addCompanyModal, setAddCompanyModal] = useState(false);
     const [editQuoteModal, setEditQuoteModal] = useState(false);
     const [editLineModal, setEditLineModal] = useState(false);
-    const [getFile, setGetFile] = React.useState(null);
-
     // const [editCostingSummaryData, seteditCostingSummaryData] = useState(null);
     const [contact, setContact] = useState();
     const [company, setCompany] = useState();
@@ -282,49 +280,13 @@ const TenderEdit = () => {
 
     }
 
-       // get Files
-       const getFiles = () => {
-        api.post('/file/getFileList',{record_id:id})
-        .then((res)=>{
-          setGetFile(res.data.data);
-        })
-    }
-
-
-    const deleteFile = (fileId) =>{
-      Swal.fire({
-        title: `Are you sure?`,
-        text: "You won't be able to revert this!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          api.post('/file/deleteFile',{media_id:fileId}).then((res)=>{
-            console.log(res)
-            Swal.fire(
-              'Deleted!',
-              'Media has been deleted.',
-              'success'
-            )
-            setViewLineModal(false)
-            window.location.reload();
-          }).catch(()=>{
-            message("Unable to Delete Media","info")
-          })
-        }
-      })
-    }
-
       useEffect(()=>{
         getCostingbySummary();
         editTenderById();
         getQuote();
         getIncharge();
         getCompany();
-        getFiles();
+       // getFiles();
         console.log(lineItem)
       },[id])
 
@@ -1645,22 +1607,9 @@ const TenderEdit = () => {
                 </Col>
                 </Row>
 
-               <AttachmentModal opportunityId={id} attachmentModal={attachmentModal} setAttachmentModal={setAttachmentModal} />
-               <table>
-               {getFile ? getFile.map(res=>{
-                        return (
-                          <>
-                          
-                            <tr>
-                              <th> <p><a href={`http://43.228.126.245/smartco-api/storage/uploads/${res.file_name}`} target="_blank" rel="noreferrer">{res.file_name}</a> </p></th>
-                              <th width="5%"></th>
-                              <th> <button type="button" className="btn btn-secondary" onClick={()=>{deleteFile(res.media_id)}}> X </button></th>
-                            </tr>
-                          
-                        </>
-                        )
-                    }) : (<p>no files uploaded yet</p>)}
-             </table>
+               <AttachmentModal opportunityId={id} attachmentModal={attachmentModal} setAttachmentModal={setAttachmentModal}  />
+               <ViewFileComponent opportunityId={id} />
+
       </TabPane>
           
           </TabContent>
